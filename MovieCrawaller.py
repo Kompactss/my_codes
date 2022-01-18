2713f36f90f34fef86505cc309f1ca20056f0788
Kompactss <33521486+Kompactss@users.noreply.github.com>
2022-01-18 18:13:11 +0900

    Delete MovieCrawaller.py

--- MovieCrawaller.py
+++
@@ -1,9 +0,0 @@
-import requests as rq
-
-res = rq.get("https://movie.naver.com/movie/point/af/list.naver?&page=1")
-res.raise_for_status()
-
-print(len(res.text))
-
-with open("res_crawl.html", "w", encoding="utf8") as f:
-    f.write(res.text)
